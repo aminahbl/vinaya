f0f44f6f2bfa1be3623dc594daef0e0d904f032f
aminahbl <aminahbl.github@37e.uk>
2023-08-06 23:11:43 +0200

    temp fix Xata type issue

--- app/components/RuleCard/RuleCard.tsx
+++ app/components/RuleCard/RuleCard.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import styles from "./RuleCard.module.css";
 import { PliTvPmBiRulesRecord } from "@xata";
 
-const RuleCard = ({ rule }: { rule: PliTvPmBiRulesRecord }) => {
+const RuleCard = ({ rule }: { rule: any }) => {
   const { title, imgId } = rule;
 
   const src = `tr:w-235,h-210,fo-auto/${process.env.NEXT_PUBLIC_IK_PM_BI_DIR}/${imgId}.png`;
