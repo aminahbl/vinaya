e27049e4fb79e91be3cce6893fe7b5d3b8027b98
aminahbl <aminahbl.github@37e.uk>
2023-08-07 15:15:52 +0200

    fix server / client mismatch & cache IKImages

--- app/components/IKImage/IKImage.tsx
+++ app/components/IKImage/IKImage.tsx
@@ -1,56 +1,47 @@
-"use client";
-
+import { useMemo } from "react";
 import Image, { type ImageProps } from "next/image";
 import crypto from "crypto";
 
-interface LoaderProps {
-  src: string;
-}
+function getIKImgSrc(path: IKImageProps["src"]) {
+  if (typeof path !== "string") {
+    return "path is not a string";
+  }
 
-function imageKitLoader({ src }: LoaderProps) {
-  const srcParts = src.split("/");
-  const params = srcParts[0];
-  const transformationPath = `${params}/${srcParts.slice(1).join("/")}`;
+  const pathParts = path.split("/");
+  const params = pathParts[0];
+  const transformationPath = `${params}/${pathParts.slice(1).join("/")}`;
 
   const imgUrl = `${
     process.env.NEXT_PUBLIC_IK_ROOT_URL ?? ""
   }/${transformationPath}`;
 
-  const expiryTimestamp =
-    parseInt((new Date().getTime() / 1000).toString(), 10) + 999999;
-
   const signature = crypto
     .createHmac("sha1", process.env.IMAGEKIT_API_KEY ?? "")
-    .update(`${transformationPath}${expiryTimestamp}`)
+    .update(`${transformationPath}${process.env.NEXT_PUBLIC_IK_TIMESTAMP}`)
     .digest("hex");
 
-  return `${imgUrl}?ik-t=${expiryTimestamp}&ik-s=${signature}`;
+  return `${imgUrl}?ik-t=${process.env.NEXT_PUBLIC_IK_TIMESTAMP}&ik-s=${signature}`;
 }
 
 interface IKImageProps extends ImageProps {
-  src: string;
+  path: string;
   alt: string;
   width: number;
   height?: number;
 }
 
 /**
- * Renders an ImageKit image component using a custom loader function returning a signed URL (eg. "https://ik.imagekit.io/tvm15igl7/tr:w-800:q-75/pli-tv-bi-vb/np001.png?ik-t=1691273508&ik-s=API_KEY").
+ * Renders an ImageKit image component from a signed `src` URL (eg. "https://ik.imagekit.io/tvm15igl7/tr:w-800:q-75/pli-tv-bi-vb/np001.png?ik-t=1691273508&ik-s=API_KEY").
  *
  * - The `src` prop is built of transformation params and image location. eg. "tr:w-800:q-75/pli-tv-bi-vb/np001.png".
  */
-const IKImage = (props: IKImageProps) => {
-  const { src, alt, width, height, ...rest } = props;
+const IKImage = async (props: IKImageProps) => {
+  const { src: path, alt, width, height, ...rest } = props;
+
+  const imgSrc = useMemo(() => getIKImgSrc(path), [path]);
 
   return (
-    <Image
-      loader={imageKitLoader}
-      src={src}
-      alt={alt}
-      width={width}
-      height={height}
-      {...rest}
-    />
+    <Image src={imgSrc} alt={alt} width={width} height={height} {...rest} />
   );
 };
 
---
+++ utils/constants.ts
@@ -0,0 +1,1 @@
+export const IK_TIMESTAMP = 2543489914
